0c915a48f48a14635b83f1db862ffdcb2ce55805
cleyxds <cleysonn@hotmail.com.br>
2021-05-23 12:29:20 -0300

    Individual card done

--- src/components/PricingTable/index.tsx
+++ src/components/PricingTable/index.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
 
-import './styles.sass';
+import styles from './styles.module.sass';
+
+import Arrow from '../../assets/arrow.png';
 
 export const PricingTable = () => {
   return (
-    <div className="test">Pricing Table</div>
+    <div className={styles.pricingContainer}>
+      <div className={styles.individual}>
+        <div className={styles.price}>
+          <span>$</span>2
+        </div>
+        <h2>Individual</h2>
+        <div className={styles.feature}>
+          <h3>Office <span>20 GB</span> </h3>
+          <div className={styles.progressOffice} />
+        </div>
+        <div className={styles.feature}>
+          <h3>Home <span>10 GB</span> </h3>
+          <div className={styles.progressHome} />
+        </div>
+        <div className={styles.continue}>
+          <button>
+            Continue
+            <img src={Arrow} alt=""/>
+          </button>
+        </div>
+      </div>
+      <div className={styles.shared}>
+        <h2>Shared</h2>
+      </div>
+      <div className={styles.unlimited}>
+        <h2>Unlimited</h2>
+      </div>
+    </div>
   )
 }
--- src/styles/GlobalStyles/index.tsx
+++ src/styles/GlobalStyles/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import './styles.sass';
+import './styles.module.sass';
 
 export const GlobalStyles = () => {
   return <></>
