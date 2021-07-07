3f9b6f7d6d6bee4b1c0e9066d9fe82dcac31f7e7
cleyxds <cleysonn@hotmail.com.br>
2021-07-07 16:33:36 -0300

    markup for input page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { useHistory } from 'react-router-dom';
+import { Header } from '../../components/Header';
 
 import { customers } from '../../services/api';
 
@@ -11,16 +13,28 @@ export const Login = () => {
   const history = useHistory();
 
   const handleFormLogin = () => {
-    
     history.push('/dashboard');
   }
 
   return (
-    <form>
-      <input type="text" placeholder="Seu nome de usuário" />
-      <input type="text" placeholder="Sua senha" />
+    <>
+      <Header />
+      <div className={styles.loginContainer}>
+        <h1>Bem vindo(a) de volta!</h1>
 
-      <button type="submit" onClick={handleFormLogin}>Login</button>
-    </form>
+        <section className={styles.formContainer}>
+          <form>
+            <input type="text" placeholder="Nome de usuário" />
+            <input type="password" placeholder="Senha" />
+
+            <button type="submit" onClick={handleFormLogin}>Login</button>
+          </form>
+        </section>
+
+        <div className={styles.alternative}>
+          <span>Não tem uma conta? <Link to="/signup">Crie uma</Link></span>
+        </div>
+      </div>
+    </>
   )
 }
