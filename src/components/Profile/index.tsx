2d17b61c65b3511a0e3867b145aa878dab87d433
cleyxds <cleysonn@hotmail.com.br>
2021-07-13 15:14:39 -0300

    profile update

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -6,12 +6,22 @@ import { customers } from '../../services/api';
 
 import styles from './profile.module.sass';
 
+interface Customer {
+  id: number
+  name: string
+  login: string
+  devices: number
+  avatar_url: string
+  created_at: string
+}
+
 export const Profile = () => {
+  const [customer, setCustomer ] = useState<Customer>()
 
   return (
     <div className={styles.profileContainer}>
-      <img src={UserIcon} alt="User" />
-      <h4>{'username'}</h4>
+      <img src={UserIcon} />
+      <h4>{customer?.name}</h4>
     </div>
   )
 }
